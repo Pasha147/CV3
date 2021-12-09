c7e53bc2d5d8afb998d50ae016673b732ddf2010
Pasha <75729255+Pasha147@users.noreply.github.com>
2021-12-09 19:54:21 +0200

    Server and Email have been created

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -46,7 +46,7 @@ export function sendMessageF(message) {
   return async (dispatch) => {
     // const url = `http://localhost:3000/api/data/${5}`;
 
-    const url = `https://myserverpf.herokuapp.com/api/data`;
+    const url = `https://fpcv.herokuapp.com/api/data/${5}`;
     const resp = await request(url, "POST", message);
     console.log("resp-->", resp);
 
